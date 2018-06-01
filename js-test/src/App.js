d62e60a9f1b447fd74c11ed790d3bdcc927b6bb7
carlosestrada22 <carlos.estradamercado@alumnos.udg.mx>
2018-06-01 09:56:59 -0500

    feat add Import components to main app

--- js-test/src/App.js
+++ js-test/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import './App.css'
 
 import MainButton from './components/main-button/main-button.js'
+import ChanginText from './components/changing-text/changing-text.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <MainButton />
+        <ChanginText />
       </div>
     );
   }
